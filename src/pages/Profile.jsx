f96402730244fe405b26418c7df72c03d9fbfccf
Nathan Buszkiewicz <Buszkiewicz@wisc.edu>
2025-07-17 02:01:12 -0500

    Fluid + color fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,17 +3,17 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default function Profile(props) {
     return (
-        <Container className ="mt-4">
+        <Container fluid className ="mt-4">
             <Row>
                 <Col xs={3}>
-                    <Card style={{ height: '100px', backgroundColor: '89CFF0'}}>
+                    <Card style={{ height: '100px', backgroundColor: '#89CFF0'}}>
                         <Card.Body>
                             This is a profile summary card
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col xs={9}>
-                    <Card style={{ height: '300px', backgroundColor: 'F88379'}}>
+                    <Card style={{ height: '300px', backgroundColor: '#F88379'}}>
                         <Card.Body>
                             This is a match history card
                         </Card.Body>
