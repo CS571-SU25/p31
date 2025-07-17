e9b9bb7cbb8c52272529d9505a496d01aa1e3fd8
Nathan Buszkiewicz <Buszkiewicz@wisc.edu>
2025-07-17 02:09:48 -0500

    Manual card width + coloring for debug

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,23 +3,23 @@ import { Row, Col, Card } from "react-bootstrap";
 
 export default function Profile(props) {
     return (
-        <div>
+        <Container fluid className="mt-4">
             <Row>
-                <Col xs={3}>
-                    <Card style={{ height: '100px', backgroundColor: '#89CFF0'}}>
+                <Col xs={3} style={{ backgroundColor: '#00008B' }}>
+                    <Card style={{ width: '100%', height: '100px', backgroundColor: '#89CFF0' }}>
                         <Card.Body>
                             This is a profile summary card
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col xs={9}>
-                    <Card style={{ height: '300px', backgroundColor: '#F88379'}}>
+                <Col xs={9} style={{ backgroundColor: '#8B0000' }}>
+                    <Card style={{ width: '100%', height: '300px', backgroundColor: '#F88379' }}>
                         <Card.Body>
                             This is a match history card
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
-        </div>
+        </Container>
     )
 }
