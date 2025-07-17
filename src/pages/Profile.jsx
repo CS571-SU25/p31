a520e71812ec0b22a8a4641bf1dfbebfd60e5515
Nathan Buszkiewicz <Buszkiewicz@wisc.edu>
2025-07-17 00:33:26 -0500

    Basic working layout for profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,25 @@
-export default function Home(props) {
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+
+export default function Profile(props) {
     return (
-        <div>
-            <h1>This is my profile page</h1>
-        </div>
+        <Container className ="mt-4">
+            <Row>
+                <Col xs={3}>
+                    <Card style={{ height: '100px', backgroundColor: '89CFF0'}}>
+                        <Card.Body>
+                            This is a profile summary card
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs={9}>
+                    <Card style={{ height: '300px', backgroundColor: 'F88379'}}>
+                        <Card.Body>
+                            This is a match history card
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
