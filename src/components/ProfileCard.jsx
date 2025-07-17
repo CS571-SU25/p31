7c44e3c507a82b9c3aa21973ea7fdbfd32e9a3d5
Nathan Buszkiewicz <Buszkiewicz@wisc.edu>
2025-07-17 04:45:51 -0500

    Display basic data. Not beautified

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,12 @@
 import {Container} from 'react-bootstrap'
 
 
-export default function ProfileCard() {
+export default function ProfileCard({username, tag, region, rank}) {
+
     return (
-        <>
-            <p>I am sick of this</p>
-        </>
+        <Container fluid style={{backgroundColor : 'lightgray', width: "100vw"}}>
+                <h1>{username}#{tag} - {region}</h1>
+                <h3>{rank}</h3>
+        </Container>
     );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,13 +3,53 @@ import {Container, Row, Col} from 'react-bootstrap'
 
 import ProfileCard from '../components/ProfileCard'
 export default function Profile(props) {
+
+    const mock = { /* Mock Data for a User */
+        summonerName : "player",
+        summonerTag : "0000",
+        region : "NA",
+        rank : "Platinum 4",
+        matchHistory: [
+            {
+                id: 0,
+                result: "win",
+                kills: 7,
+                deaths: 3,
+                assits: 10,
+                creepScore: 252,
+                visonScore: 48,
+                gameDuration: 2128 //seconds
+            },
+            {
+                id: 1,
+                result: "win",
+                kills: 11,
+                deaths: 4,
+                assits: 7,
+                creepScore: 190,
+                visonScore: 24,
+                gameDuration: 1231 //seconds
+            },
+            {
+                id: 2,
+                result: "loss",
+                kills: 3,
+                deaths: 5,
+                assits: 2,
+                creepScore: 169,
+                visonScore: 31,
+                gameDuration: 1352 //seconds
+            }
+        ]
+    };
     return (
         <>
             <Container fluid style={{
-                width: "100vw" }}>
+                width: "100vw",
+                padding: "0" }}>
                 <Row className="justify-content-md-center">
                     <Col md="auto">
-                        <ProfileCard />
+                        <ProfileCard username={mock.summonerName} tag={mock.summonerTag} region={mock.region} rank={mock.rank}/>
                     </Col>
                 </Row>
             </Container>
